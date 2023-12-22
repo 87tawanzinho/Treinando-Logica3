91b5e401ce012167b3f9535ade25d257d405ab62
87tawanzinho <87tawanzinho@gmail.com>
2023-12-22 00:25:28 +0000

    sort

---
+++ sort.js
@@ -0,0 +1,40 @@
+const characters = [
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: "188",
+    mass: "84",
+    eye_color: "blue",
+    gender: "male",
+  },
+];
+
+const x = characters.sort((a, b) => {
+  if (a.height < b.height) {
+    return -1;
+  } else {
+    return true;
+  }
+});
+
+console.log(x);
